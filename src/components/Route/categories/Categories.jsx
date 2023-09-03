7a230dcefda5caa1a34c8a1950b48b5f35956a48
moosa <moosaabdullahi45@gmail.com>
2023-09-03 18:40:38 +0200

    new

--- src/components/Route/categories/Categories.jsx
+++ src/components/Route/categories/Categories.jsx
@@ -8,13 +8,13 @@
 //   return (
 //   <>
 //   <div className={`${styles.section} hidden sm:block`}>
-//     <div className="branding my-12 flex justify-between w-full shadow-sm bg-white p-5  rounded-md">
+//     <div className="flex justify-between w-full p-5 my-12 bg-white rounded-md shadow-sm branding">
 //         {
 //             brandingData && brandingData.map((i, index) => (
 //                 <div className="flex items-start" key={index}>
 //                     {i.icon}
 //                     <div className="px-3">
-//                         <h3 className="font-bold text-sm md:text-base">
+//                         <h3 className="text-sm font-bold md:text-base">
 //                             {i.title}
 
 //                         </h3>
@@ -70,7 +70,7 @@
 //   return (
 //     <>
 //       <div className={`${styles.section} hidden sm:block`}>
-//         <div className="branding my-12 flex justify-between w-full shadow-sm bg-white p-5  rounded-md">
+//         <div className="flex justify-between w-full p-5 my-12 bg-white rounded-md shadow-sm branding">
 //           {/* Your brandingData mapping here */}
 //           {/* ... */}
 //         </div>
@@ -191,13 +191,13 @@ const Categories = () => {
   return (
     <>
       <div className={`${styles.section} hidden sm:block`}>
-        <div className="branding my-12 flex justify-between w-full shadow-sm bg-white p-5  rounded-md">
+        <div className="flex justify-between w-full p-5 my-12 bg-white rounded-md shadow-sm branding">
           {brandingDatas &&
             brandingDatas.map((brand) => (
               <div className="flex items-start" key={brand.title}>
                 {brand.icon}
                 <div className="px-3">
-                  <h3 className="font-bold text-sm md:text-base">
+                  <h3 className="text-sm font-bold md:text-base">
                     {brand.title}
                   </h3>
                   <p className="text-xs md:text-sm">{brand.Description}</p>
@@ -211,7 +211,7 @@ const Categories = () => {
         className={`${styles.section} bg-white p-6 rounded-lg mb-12`}
         id="categories"
       >
-        <h2 className="text-xl font-semibold text-gray-800 mb-4">
+        <h2 className="mb-4 text-xl font-semibold text-gray-800">
           Categories in Pharmacy
         </h2>
         <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 lg:grid-cols-3 xl:grid-cols-4">
@@ -222,9 +222,9 @@ const Categories = () => {
               {categoriesData &&
                 categoriesData.map((category) => (
                   <div
-                    className="flex flex-col items-center p-4 cursor-pointer rounded-lg shadow-md hover:shadow-lg transform transition-all"
+                    className="flex flex-col items-center p-4 transition-all transform rounded-lg shadow-md cursor-pointer hover:shadow-lg"
                     key={category.id}
-                    onClick={() => handleCategoryClick(category.id)}
+                    onClick={() => handleCategoryClick(category.name)}
                   >
                     <img
                       src={category.image}
